71e5f52523a648129a2cfdbf75361d2aacfcc7a8
OoDone <48130949+OoDone@users.noreply.github.com>
2021-11-23 12:42:56 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,38 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 /*task name:
 description/details:
 when needed(due date):
 */
 client.on('message', async function(msg) {
+  var quiz;
+  var answer;
   if (msg.content.startsWith('?sendtask')) {
-    var split;
     msg.channel.send("test");
-    if(msg.content.includes("\n")) {split = msg.content.split('\n');}
+    if(msg.content.includes("\n")) {var split = msg.content.split('\n'); return;}
     msg.channel.send("test: " + split[0]);
-    if (split[0] != null) {var task = split[0];}
-    if (split[1] != null) {var details = split[1];}
-    if (split[2] != null) {var date = split[2];} else {msg.channel.send("Invalid Arguments: Format: \n ?sendtask <Task Name> \n <details> \n <date needed>");}
+    if (split[0] != null) {return;} else {msg.channel.send("Invalid Arguments: Format: \n ?sendtask <Task Name> \n <details> \n <date needed>");}
+    if (split[1] != null) {return;} else {msg.channel.send("Invalid Arguments: Format: \n ?sendtask <Task Name> \n <details> \n <date needed>");}
+    if (split[2] != null) {return;} else {msg.channel.send("Invalid Arguments: Format: \n ?sendtask <Task Name> \n <details> \n <date needed>");}
+    var task = split[0] + ":" + split[1] + ":" + split[2];
+    msg.channel.send("Msg: " + task);
+  }
+  if (msg.content.startsWith('?sendtask2')) {
+    msg.channel.send("What is the task name?");
+    quiz = true;
+    //await answer != null;
+    await question1();
+    msg.channel.send("Answer: " + answer);
+  }
+  if (quiz == true) {
+    if (!msg.content.startsWith('?sendtask')) {
+      answer = msg.content;
+    }
   }
 });
+function question1() {
+  if (answer != null) {
+    return;
+  }
+}
+
