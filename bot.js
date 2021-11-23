90e4e48a9b7bef07593406ab6a379d9f896aecb8
OoDone <48130949+OoDone@users.noreply.github.com>
2021-11-23 11:03:33 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.on('message', async function(msg) {
+  if (msg.content.startsWith('?ping')) {
+    msg.channel.send("xd");
+    
+  }
+});
